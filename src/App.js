764be28a62e7751f1c1a16cbe125becedc7e489c
Emmanuel <emitoz2012@gmail.com>
2021-09-27 21:12:15 +0100

    Redirect auth user

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
-import { HomePage } from "./pages/homepage";
-import "./assets/fonts/feather.css";
+import { useContext } from "react";
+import { Redirect } from "react-router";
 import { Route, Switch } from "react-router-dom";
+
+import { HomePage } from "./pages/homepage";
 import { Login } from "./pages/auth/login";
 import { Register } from "./pages/auth/register";
 import { Checkout } from "./pages/checkout";
 import { CategoryProducts } from "./pages/category-products";
 import { Main } from "./Main";
-import { AuthProvider } from "./context/auth";
-import { CartProvider } from "./context/cart";
 import { ProductDetails } from "./pages/product-details";
 
+import { AuthContext } from "./context/auth";
+import { CartProvider } from "./context/cart";
 
-function App() {
+import "./assets/fonts/feather.css";
+
+const App = () => {
+
+  const { currentUser } = useContext(AuthContext)
+
   return (
     <Switch>
-      <AuthProvider>
-        <CartProvider>
-          <Route exact path="/"><Main Content={HomePage}/></Route>
-          <Route path="/checkout"><Main Content={Checkout}/></Route>
-          <Route path="/categories/:category"><Main Content={CategoryProducts}/></Route>
-          <Route path="/products/:product"><Main Content={ProductDetails}/></Route>
-          
-          <Route path="/login"><Login /></Route>
-          <Route path="/register"><Register /></Route>
-        </CartProvider>
-      </AuthProvider>
+      <CartProvider>
+        <Route exact path="/"><Main Content={HomePage}/></Route>
+        <Route path="/checkout"><Main Content={Checkout}/></Route>
+        <Route path="/categories/:category"><Main Content={CategoryProducts}/></Route>
+        <Route path="/products/:product"><Main Content={ProductDetails}/></Route>     
+        <Route exact path="/login" render={() => currentUser ? <Redirect to="/" /> : <Login />}></Route>
+        <Route exact path="/register" render={() => currentUser ? <Redirect to="/" /> : <Register />}></Route>
+      </CartProvider>
     </Switch>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
+import { AuthProvider } from './context/auth';
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
